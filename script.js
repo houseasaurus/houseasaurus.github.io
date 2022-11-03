07276ec4147aff86a41c0384cbba5fda36785ea2
Amber Huo <amberhuo2012@gmail.com>
2022-11-03 01:55:14 -0400

    mess

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  console.log('togled');
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
 //For loading animation 
 window.addEventListener("load", () => {
-    document.querySelector("body").classList.add("loaded"); 
+  document.querySelector("body").classList.add("loaded"); 
 });
+
+// Header change style on scroll
+window.onscroll = function() {scrollFunc()};
+
+function scrollFunc() {
+  if (document.body.scrollTop > 50) {
+    document.getElementById("myTopnav").classList.add("active");
+  } else {
+    document.getElementById("myTopnav").classList.remove("active");
+  }
+}
